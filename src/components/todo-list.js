809c1a942b3117321f2cd6fb5ae42533eeb1970c
sevendc <benford.mark783@gmail.com>
2019-07-04 22:43:24 +0400

    Importing CSS

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,23 +1,23 @@
 import React from 'react';
+
+import TodoListItem from './todo-list-item';
 import './todo-list.css';
 
-import TodoListItem from "./todo-list-item";
+const TodoList = ({ todos }) => {
 
-const TodoList = ({todos}) => {
-
-  const elements = todos.map((item)=> {
-    
-    const {id, ...itemProps} = item;
+  const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
 
     return (
       <li key={id} className="list-group-item">
-        <TodoListItem {...itemProps}/></li>
+        <TodoListItem {...itemProps } />
+      </li>
     );
   });
 
   return (
     <ul className="list-group todo-list">
-      {elements}
+      { elements }
     </ul>
   );
 };
